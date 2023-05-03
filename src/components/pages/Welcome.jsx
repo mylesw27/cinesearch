301c9e74dafc29e71a98b851543459007fb49f64
dxk0ta <dxkotacamps@gmail.com>
2023-05-02 19:36:49 -0700

    updated logo

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -2,10 +2,10 @@ import logo from "../assets/Logo.png"
 
 export default function Welcome() {
 	return (
-		<div>
+		<div className="Welcome-page">
 			<img src={logo} alt="Logo" />
-			<h1>Welcome to CineSearch</h1>
-			<p>this is the landing page</p>
+			<h1>Welcome to CineSearch!</h1>
+			<p>I don't mean to come off too cocky but I'm kind of the new "it" app.. Tell your friends.</p>
 		</div>
 	)
 }
