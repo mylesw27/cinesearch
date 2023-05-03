b80998f9f0f829c96a843a728bfac8a627707570
mylesw27 <myleswiegel27@gmail.com>
2023-05-02 21:12:47 -0700

    Add details link to cards

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Card(props) {
     const movie = props.movie
     console.log(movie)
     const posterUrl = `https://image.tmdb.org/t/p/w780/${movie.poster_path}`;
     return (
-        <div className="card">
-            <img src={posterUrl} />
-            <h2>{movie.name}</h2>
-            <p>{movie.overview}</p>
-        </div>
+        <>
+            <Link to={`/movies/${movie.id}/details`}>
+                <div className="card">
+                    <img src={posterUrl} />
+                    <h2>{movie.name}</h2>
+                    <p>{movie.overview}</p>
+                </div>
+            </Link>
+        </>
     )
 }
