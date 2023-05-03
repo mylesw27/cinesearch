c1c6f80d1f431bc58841c981337434cd72fce09e
dxk0ta <dxkotacamps@gmail.com>
2023-05-02 19:16:16 -0700

    starting welcome page and got logo on screen

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,7 +1,11 @@
+import logo from "../assets/Logo.png"
+
 export default function Welcome() {
 	return (
 		<div>
-			hello from welcome
+			<img src={logo} alt="Logo" />
+			<h1>Welcome to CineSearch</h1>
+			<p>this is the landing page</p>
 		</div>
 	)
 }
