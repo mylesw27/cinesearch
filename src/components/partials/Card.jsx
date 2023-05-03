2ae4a1f03cd93a0f67826e0883cd571e5d801924
mylesw27 <myleswiegel27@gmail.com>
2023-05-03 12:24:01 -0700

    Update favorites.js

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -4,6 +4,16 @@ export default function Card(props) {
     const movie = props.movie
     console.log(movie)
     const posterUrl = `https://image.tmdb.org/t/p/w780/${movie.poster_path}`;
+
+    const watchArray = watchMovie.map((service) => {
+        return (
+            <>
+                <img src={`https://image.tmdb.org/t/p/w200/${service.logo_path}`} />
+                <p>{service.provider_name}</p>
+            </>
+        )
+    })
+
     return (
         <>
             <Link to={`/movies/${movie.id}/details`}>
