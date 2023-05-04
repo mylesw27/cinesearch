1dec9ce191ff7b28d4a619ef42d8de245086cb68
dxk0ta <dxkotacamps@gmail.com>
2023-05-04 11:24:42 -0700

    started on styling

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -26,15 +26,46 @@ export default function Header({ currentUser, handleLogout }) {
 			</Link> 
 		</>
 	)
+import { Link } from 'react-router-dom'
+
+export default function Header({ currentUser, handleLogout }) {
+	const loggedIn = (
+		<>
+			{/* if the user is logged in... */}
+			<Link to="/">
+				<span onClick={handleLogout}>logout</span>
+			</Link>
+
+			<Link to="/profile">
+				Profile
+			</Link>
+		</>
+	)
+
+	const loggedOut = (
+		<>
+			{/* if the user is not logged in... */}
+			<Link to="/register">
+				Register
+			</Link> {" | "}
+
+			<Link to="/login">
+				Login
+			</Link> 
+		</>
+	)
 
 	return (
 		<nav class="navbar navbar-light bg-light header">
             <div>
+            <a class="navbar-brand navbar-link" href="/">
+                    Home
+                </a>
                 <a class="navbar-brand navbar-link" href="/register">
-                    REGISTER
-                </a> {" | "}
+                    Register
+                </a>
                 <a class="navbar-brand navbar-link" href="/login">
-                    LOGIN
+                    Login
                 </a>
             </div>
             <img class="navbar-logo" src="/logo.png" alt="logo image" />
