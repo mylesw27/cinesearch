19ac47491b42d5207cc983394154883eb6a21153
dxk0ta <dxkotacamps@gmail.com>
2023-05-04 15:37:52 -0700

    working on welcome page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,9 +1,11 @@
-
+import myImage from "../assets/movienight.png.jpg"
 
 export default function Welcome() {
 	return (
-		<div className="Welcome-page">
-			  <img src="../assets/movienight.png" alt="Welcome Image" class="welcome-image" />
-		</div>
+		<div style={{ position: "relative" }}>
+      <img className="movienight-img" src={myImage} alt="My Image" style={{ width: "1200px", height: "740px", marginTop: "140px", marginLeft: "-270px" }} />
+      <h1 style={{ position: "absolute", top: "50px", left: "50%", transform: "translate(-50%, -50%)", fontFamily:"Abril Fatface", fontSize: "xxx-large"}}>LIGHTS, CAMERA, ACTION! </h1>
+	  <h2 style={{ position: "absolute", top: "70px", left: "80%", transform: "translate(-50%, -50%)", fontFamily:"Abril Fatface", fontSize: "xxxx-large" }}> Join us for the ultimate movie experience. </h2>
+    </div>
 	)
 }
