b56bdd1e28c86de98c3ead1a4c6ae85e72aaf4fa
dxk0ta <dxkotacamps@gmail.com>
2023-05-05 11:06:44 -0700

    fixing merge conflicts

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -26,34 +26,6 @@ export default function Header({ currentUser, handleLogout }) {
 			</Link> 
 		</>
 	)
-import { Link } from 'react-router-dom'
-
-export default function Header({ currentUser, handleLogout }) {
-	const loggedIn = (
-		<>
-			{/* if the user is logged in... */}
-			<Link to="/">
-				<span onClick={handleLogout}>logout</span>
-			</Link>
-
-			<Link to="/profile">
-				Profile
-			</Link>
-		</>
-	)
-
-	const loggedOut = (
-		<>
-			{/* if the user is not logged in... */}
-			<Link to="/register">
-				Register
-			</Link> {" | "}
-
-			<Link to="/login">
-				Login
-			</Link> 
-		</>
-	)
 
 	return (
 		<nav class="navbar navbar-light bg-light header">
