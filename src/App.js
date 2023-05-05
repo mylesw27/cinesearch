6e8cae9b0d5b9608600ae67643bf614570636464
mylesw27 <myleswiegel27@gmail.com>
2023-05-04 23:36:08 -0700

    add route for search page

--- src/App.js
+++ src/App.js
@@ -49,26 +49,19 @@ function App() {
       <Header />
       <div className="App">
         <Routes>
-          <Route path="/" element={<Welcome />} />
+          <Route
+            path="/"
+            element={<Welcome />}
+          />
 
           <Route
             path="/register"
-            element={
-              <Register
-                currentUser={currentUser}
-                setCurrentUser={setCurrentUser}
-              />
-            }
+            element={<Register currentUser={currentUser} setCurrentUser={setCurrentUser} />}
           />
 
           <Route
             path="/login"
-            element={
-              <Login
-                currentUser={currentUser}
-                setCurrentUser={setCurrentUser}
-              />
-            }
+            element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser} />}
           />
 
           <Route
@@ -113,9 +106,10 @@ function App() {
 
           <Route
             path="/watchlist"
-            element={
-              <Watchlist listName={"Watchlist"} currentUser={currentUser} />
-            }
+            element={<Watchlist
+              listName={"Watchlist"}
+              currentUser={currentUser}
+            />}
           />
 
           <Route
