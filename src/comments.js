7cfb43c7e0de46b5ea0e6206b1b8757865d24ab5
mylesw27 <myleswiegel27@gmail.com>
2023-05-05 21:37:25 -0700

    Add commentcard partial component

---
+++ src/comments.js
@@ -0,0 +1,28 @@
+const comments = [
+    {
+        "tmdbId:": "640146",
+        "userId": "6432nvzxvaqwepljvwe-ae",
+        "userName": "movie_snob22",
+        "commentBody": "omg so brave",
+    },
+    {
+        "tmdbId:": "640146",
+        "userId": "asdvasxcvqwefa",
+        "userName": "commenter55",
+        "commentBody": "🔥🔥🔥🔥🔥",
+    },
+    {
+        "tmdbId:": "502356",
+        "userId": "asdasdvqwe3rxcvqwefa",
+        "userName": "CommentGuy",
+        "commentBody": "So great!1!!",
+    },
+    {
+        "tmdbId:": "502356",
+        "userId": "aasdfbvqwe3rxcvqwefa",
+        "userName": "jokester",
+        "commentBody": "just here for the lolz",
+    },
+]
+
+export default comments
---
+++ src/components/partials/CommentCard.jsx
@@ -0,0 +1,12 @@
+// imports
+
+// export with comment in object destructing
+export default function CommentCard() {
+
+    return (
+        <div>
+            <h3>username</h3>
+            <p>Here's where the body of the comment will go</p>
+        </div>
+    )
+}
