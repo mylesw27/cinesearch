09f56bf9632b17d685e6556d38258fd62488c5ea
mylesw27 <myleswiegel27@gmail.com>
2023-05-09 13:48:33 -0700

    Adjust button postioning on movie details

--- src/components/pages/MovieDetails.jsx
+++ src/components/pages/MovieDetails.jsx
@@ -114,7 +114,7 @@ function MovieDetails({ currentUser }) {
               <WatchlistButton movie={movie} watchObjId={watchObjId} currentUser={currentUser} />
             </div>
           </div>
-          <p className="stream-btn" onClick={toggleMenu} style={{ fontFamily: "Sigmar", fontWeight: "lighter", padding: "30px" }}>Click Here To Find Potential Streams</p>
+          <p className="stream-btn" onClick={toggleMenu} style={{ fontFamily: "Sigmar", fontWeight: "lighter", padding: "30px" }}>Stream It</p>
           {/* <p>Movie Homepage: {movie.homepage}</p> */}
           {isMenuOpen && (
             <div className="movie-dropdown">
